8287b9e27085c47157c6675dd328744170ea977e
Håkan MacLean <hakan@maclean.se>
2016-07-13 15:45:19 +0200

    Missing files in previous check-in.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { HTTP_PROVIDERS } from '@angular/http';
+import 'rxjs/Rx';   // Load all features
 import { RealtyListComponent } from './realty/realty-list.component';
+import { RealtyListingsService } from './realty/realty-listings.service';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   directives: [RealtyListComponent],
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  providers: [RealtyListingsService, HTTP_PROVIDERS]
 })
 export class AppComponent {
   title = 'Komuter';
